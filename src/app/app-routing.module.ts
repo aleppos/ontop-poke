2bce151ccd25e888c50ca9dbdb8c11977b5134a1
Emad Aldeen Hajjar <emadaldeen.hajjar@gmail.com>
2023-08-26 13:47:31 -0300

    chore: build app structures (WIP)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { PokemonListComponent } from './screens/pokemon-list/pokemon-list.component';
+import { AppRoutes } from 'src/constants/AppRoutes';
+import { PokemonDetailsComponent } from './screens/pokemon-details/pokemon-details.component';
+import { NotFoundComponent } from './screens/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: AppRoutes.POKEMON_LIST,
+        component: PokemonListComponent,
+      },
+      {
+        path: AppRoutes.POKEMON_DETAILS,
+        component: PokemonDetailsComponent,
+      },
+      {
+        path: '',
+        redirectTo: AppRoutes.POKEMON_LIST,
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { provideQueryClientOptions } from '@ngneat/query';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PokemonListComponent } from './screens/pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './screens/pokemon-details/pokemon-details.component';
+import { NotFoundComponent } from './screens/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PokemonListComponent,
+    PokemonDetailsComponent,
+    NotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideQueryClientOptions({
